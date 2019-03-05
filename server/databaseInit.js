a3d1b05205e51078539703a3a9b867d4e1d889f1
Alexander Gusev <san4es021@gmail.com>
2019-03-05 18:07:30 +0300

    sad

--- server/databaseInit.js
+++ server/databaseInit.js
@@ -16,16 +16,9 @@ const settings =
 
 mongoose
   .connect(
-    'mongodb://46.101.24.197:27017/cybpoint',
-    // 'mongodb://127.0.0.1:27017/cybpoint',
-    {
-      useNewUrlParser: true
-      // user: 'Cybpoint',
-      // pass: password,
-      // auth: {
-      //   authSource: 'admin'
-      // }
-    }
+    // 'mongodb://46.101.24.197:27017/cybpoint',
+    'mongodb://127.0.0.1:27017/cybpoint',
+    settings
   )
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.log('Error occured connecting to MongoDB:', err))

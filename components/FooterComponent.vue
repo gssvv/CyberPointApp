2bc3fe73e848c43c5e706b013db21c1065bd3735
Alexander Gusev <san4es021@gmail.com>
2019-03-06 18:21:39 +0300

    Update FooterComponent.vue

--- components/FooterComponent.vue
+++ components/FooterComponent.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="l-footer">
     <div class="container">
-      <div class="l-content wrapper">All rights reserved © 2019
+      <div class="l-content wrapper">
+        <div class="item">admin@cybpoint.ru</div>
+        <div class="item">CyberPoint © {{ currentYear }}</div>
+        <div class="item">Сайт исользует Cookie-файлы</div>
         <!-- Add something -->
       </div>
     </div>
@@ -9,15 +12,37 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      currentYear: new Date().getFullYear()
+    }
+  }
+}
 </script>
 
 <style lang="sass" scoped>
 @import '@/assets/style/_variables.sass'
 
 .l-footer
-  background-color: $darkBlue1
+  background-color: $darkBlue2
   padding: 30px 0
+  box-shadow: 0 -2px 3px rgba(0, 0, 0, 0.12)
   .wrapper
     text-align: center
+    display: grid
+    grid-auto-flow: column
+    justify-content: space-between
+    align-items: center
+    grid-auto-columns: 1fr
+    color: rgba(255,255,255,.3)
+    font-size: .9rem
+    .item:nth-child(1)
+      justify-self: flex-start
+    .item:nth-child(2)
+      justify-self: center
+      font-size: 1rem
+      color: #fff
+    .item:nth-child(3)
+      justify-self: flex-end
 </style>
